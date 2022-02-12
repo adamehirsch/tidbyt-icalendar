76bd6fcb3810b3360937a07bc2d2d3c616c4e86a
Adam Hirsch <adam.e.hirsch@gmail.com>
2022-02-11 19:55:13 -0600

    only draw freebusy hours on blank canvas

--- draw_freebusy.py
+++ draw_freebusy.py
@@ -41,7 +41,7 @@ def draw_week_events(img, events, image_name):
         shift_end = e.decoded("dtend") - tf
 
         shift_duration = e.decoded("dtend") - e.decoded("dtstart")
-
+        logging.debug(f"{shift_start} {shift_end} {e.decoded('summary')}")
         days_forward = shift_start.days
 
         x_start = days_forward * 9 + 1
@@ -61,14 +61,26 @@ def draw_week_events(img, events, image_name):
             # and another at the top of the next day, unless it's the last day
             if shift_end.days < 7:
                 d.rectangle([x_start + 9, 8, x_end + 9, y_end], fill=FBCOLOR)
+
         # this is meant to put the length of the shift above the block.
         # Don't insert it if the column starts too high (for a weirdly early shift start)
         if y_start > 14:
+            text_x = x_start + (2 if (shift_duration.seconds // 3600) < 10 else 0)
+            text_y = y_start - 6
+            # if img.getpixel((text_x, text_y)) != (0, 0, 0, 0):
+            #     continue
+            # print(
+            #     "PIX VALUE:",
+            #     img.getpixel((text_x, text_y)),
+            #     e.decoded("dtstart"),
+            #     e.decoded("summary"),
+            # )
+
             d.text(
                 xy=(
                     # center single digits over the column
-                    x_start + (2 if (shift_duration.seconds // 3600) < 10 else 0),
-                    y_start - 6,
+                    text_x,
+                    text_y,
                 ),
                 text=str(shift_duration.seconds // 3600),
                 font=teenyfont,
